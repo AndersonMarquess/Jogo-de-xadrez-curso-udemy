cbfbb8323ff6d96541f93900b12f54dd9b60ff41
Anderson Marques <andersongabriel96.ag@gmail.com>
2018-03-18 18:26:22 -0300

    Criado classe posicao xadrez

---
+++ Jogo-de-Xadrez/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez {
+    class PosicaoXadrez {
+
+        public char posicao { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char posicao, int linha) {
+            this.posicao = posicao;
+            this.linha = linha;
+        }
+
+        //Retorna uma posição valida, lembrando que 'a' tem o valor 1, 'b' é 2...
+        public Posicao toPosicao() {
+            return new Posicao(8 - linha, posicao - 'a');
+        }
+
+        public override string ToString() {
+            return ""+posicao+linha;
+        }
+    }
+}
