2cab778dd6d20196d260d8debd54ccc4dc6cdc40
Anderson Marques <andersongabriel96.ag@gmail.com>
2018-03-18 16:43:56 -0300

    Criado camada de tabuleiro e classe Posicao

--- Jogo-de-Xadrez/Program.cs
+++ Jogo-de-Xadrez/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using tabuleiro;
 
 namespace Jogo_de_Xadrez
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    class Program {
+        static void Main(string[] args) {
+            Posicao p;
+            p = new Posicao(1,1);
+
+            Console.WriteLine(p);
+            Console.ReadLine();
         }
     }
 }
---
+++ Jogo-de-Xadrez/tabuleiro/Posicao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace tabuleiro
+{
+    class Posicao {
+        private int linha;
+        private int coluna;
+
+        public Posicao(int linha, int coluna) {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        public int Linha { get => linha; set => linha = value; }
+        public int Coluna { get => coluna; set => coluna = value; }
+
+        public override string ToString() {
+            return linha + ", "+coluna;
+        }
+
+    }
+}
